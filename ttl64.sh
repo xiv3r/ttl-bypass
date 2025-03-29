792a3d512286b2954124b1904347cae0c9e1b37a
xiv3r <117867334+xiv3r@users.noreply.github.com>
2025-03-29 10:14:55 +0800

    Update ttl64.sh

--- ttl64.sh
+++ ttl64.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-nft 'add table inet mangle'
 nft 'add chain inet mangle mangle_prerouting_ttl64 { type filter hook prerouting priority 300; policy accept; }'
 nft 'add rule inet mangle mangle_prerouting_ttl64 ip ttl set 64'
 nft 'add rule inet mangle mangle_prerouting_ttl64 ip6 hoplimit set 64'
